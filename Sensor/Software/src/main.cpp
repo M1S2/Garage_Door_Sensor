7379676fd1c9d3163dad9160043240ffd88fdf49
Markus Scheich <markus.scheich@gmx.de>
2024-02-19 21:24:24 +0100

    Updated sensor code (loop over all wifi channels while sending data until success)

--- Sensor/Software/src/main.cpp
+++ Sensor/Software/src/main.cpp
@@ -14,6 +14,10 @@
 #define MCU_LATCH_PIN       12    // GPIO 12
 #define DOOR_SWITCH_PIN     14    // GPIO 14
 #define MAX_SEND_RETRIES    10
+#define MAX_WIFI_CHANNELS   13    // 13 in Europe, 11 for North America (adapt the following list accordingly)
+uint8_t const wifi_channel_order[] = { 1, 6, 11, 2, 3, 4, 5, 7, 8, 9, 10, 12, 13};      // First try to use the channels 1, 6 and 11 to send (these are the most assigned ones by routers)
+
+#define DEBUG_OUTPUT              // enable this define to output debugging output on the serial. If this is disabled, no serial output is used at all (to save power)
 
 ADC_MODE(ADC_VCC);
 
@@ -30,38 +34,96 @@ message_t sensor_message;
 bool messageSentReady;
 bool messageSentSuccessful;
 
-void messageSent(uint8_t *macAddr, uint8_t status)
+void OnDataSent(uint8_t* macAddr, uint8_t status)
 {
   messageSentSuccessful = (status == 0);
   messageSentReady = true;
 }
 
-// send the sensor status to the indoor station and repeat MAX_SEND_RETRIES times until the sending was successful
-void sendSensorData()
+void OnDataRecv(uint8_t* mac, uint8_t* incomingData, uint8_t len)
 {
-  uint8_t loop_cnt = 0;
-  do
-  {
-    loop_cnt++;
-    //Serial.printf("Sender loop %d: ", loop_cnt);
-    messageSentReady = false;
-    sensor_message.voltageVcc = ESP.getVcc() / 1000.00;
-    sensor_message.pinState = (digitalRead(DOOR_SWITCH_PIN) == HIGH);
-    sensor_message.numberSendLoops = loop_cnt;
-    esp_now_send(indoor_station_mac, (uint8_t *) &sensor_message, sizeof(sensor_message));
-    while(messageSentReady == false) { delay(1); /* wait here. */ }
-  }while(messageSentSuccessful == false && loop_cnt < MAX_SEND_RETRIES);
+  
+}
 
-  if(loop_cnt == MAX_SEND_RETRIES)
+/**
+ * Reinitialize ESP-NOW to the requested WiFi channel.
+ * @param wifiChannel WiFi channel that is used for the ESP-NOW connection. This must match the receiver channel that depends on the channel used by the router.
+ */
+void initEspNow(uint8_t wifiChannel)
+{
+  // clean esp now
+  esp_now_deinit();
+  WiFi.mode(WIFI_STA);
+  // set WiFi channel   
+  wifi_promiscuous_enable(1);
+  wifi_set_channel(wifiChannel);
+  wifi_promiscuous_enable(0);
+
+  //#ifdef DEBUG_OUTPUT
+  //WiFi.printDiag(Serial);
+  //#endif
+  WiFi.disconnect();
+
+  // Init ESP-NOW
+  if (esp_now_init() != 0) 
   {
-    Serial.printf("Maximum number of send retries reached (%d)\n", MAX_SEND_RETRIES);
-    digitalWrite(LED_BUILTIN, HIGH);      // LED off
+    #ifdef DEBUG_OUTPUT
+    Serial.println("Error initializing ESP-NOW");
+    #endif
   }
-  else
+  esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
+  // set callback routines
+  esp_now_register_send_cb(OnDataSent);
+  esp_now_register_recv_cb(OnDataRecv);
+
+  if(esp_now_add_peer(indoor_station_mac, ESP_NOW_ROLE_COMBO, wifiChannel, NULL, 0) != 0)
   {
-    Serial.printf("Send Success after %d loop(s)\n", loop_cnt);
-    digitalWrite(LED_BUILTIN, LOW);       // LED on
+    #ifdef DEBUG_OUTPUT
+    Serial.println("Failed to add peer");
+    #endif
   }
+}
+
+/**
+ * Send the sensor status to the indoor station and repeat MAX_SEND_RETRIES times until the sending was successful. Also loop all channels when not successful.
+ * @return return true if send success; otherwise false
+ */
+bool sendSensorData()
+{
+  sensor_message.voltageVcc = ESP.getVcc() / 1000.00;
+  sensor_message.pinState = (digitalRead(DOOR_SWITCH_PIN) == HIGH);
+  
+  for(uint8_t wifiChannelIndex = 0; wifiChannelIndex < MAX_WIFI_CHANNELS; wifiChannelIndex++)
+  {
+    uint8_t wifiChannel = wifi_channel_order[wifiChannelIndex];
+
+    initEspNow(wifiChannel);
+    uint8_t loop_cnt = 0;
+    do
+    {
+      loop_cnt++;
+      messageSentReady = false;
+      sensor_message.numberSendLoops = loop_cnt + wifiChannelIndex * MAX_SEND_RETRIES;
+      esp_now_send(indoor_station_mac, (uint8_t *) &sensor_message, sizeof(sensor_message));
+      while(messageSentReady == false) { delay(1); /* wait here. */ }
+    }while(messageSentSuccessful == false && loop_cnt < MAX_SEND_RETRIES);
+
+    if(loop_cnt == MAX_SEND_RETRIES)
+    {
+      #ifdef DEBUG_OUTPUT
+      Serial.printf("Maximum number of send retries reached (%d) on channel %d. Move to next channel.\n", MAX_SEND_RETRIES, wifiChannel);
+      #endif
+    }
+    else
+    {
+      #ifdef DEBUG_OUTPUT
+      Serial.printf("Send Success after %d loop(s) over channel %d\n", loop_cnt, wifiChannel);
+      Serial.printf("Total number of send tries = %d\n", sensor_message.numberSendLoops);
+      #endif
+      return true;    // break the wifiChannel for loop and return true to signal send success
+    }
+  }
+  return false;       // if ended here, the data couldn't be send on any channel
 }
 
 void setup()
@@ -71,30 +133,16 @@ void setup()
 
   pinMode(DOOR_SWITCH_PIN, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);      // LED off
+  digitalWrite(LED_BUILTIN, LOW);      // LED on
 
+  #ifdef DEBUG_OUTPUT
   Serial.begin(115200);
-  WiFi.mode(WIFI_STA);
   Serial.print("My MAC-Address: ");
   Serial.println(WiFi.macAddress());
+  #endif
+
+  WiFi.mode(WIFI_STA);
   WiFi.disconnect();
-  
-  if (esp_now_init() == 0)
-  {
-    Serial.println("ESPNow Init success");
-  }
-  else 
-  {
-    Serial.println("ESPNow Init fail");
-    return;
-  }
-    
-  esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
-  if(esp_now_add_peer(indoor_station_mac, ESP_NOW_ROLE_COMBO, 0, NULL, 0) != 0)
-  {
-    Serial.println("Failed to add peer");
-  }
-  esp_now_register_send_cb(messageSent);
 
   sendSensorData();
   delay(3000);
@@ -104,6 +152,5 @@ void setup()
  
 void loop()
 {
-  //sendSensorData();
-  //delay(3000);
+
 }
